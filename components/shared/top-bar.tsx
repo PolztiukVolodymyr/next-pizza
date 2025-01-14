4c754d57c79dfd3577b9dc3ec3f50de21d5c00d6
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2025-01-14 22:07:53 +0200

    start filter

---
+++ components/shared/top-bar.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { cn } from "@/lib/utils";
+import Container from "./container";
+import Categories from "./categories";
+import SortPopup from "./sort-popup";
+// import { Category } from "@prisma/client";
+
+type TopBarProps = {
+    // categories: Category[];
+    className?: string;
+};
+
+const TopBar: FC<TopBarProps> = ({ className }) => {
+    return (
+        <div
+            className={cn(
+                "sticky top-0 bg-white py-2 shadow-lg shadow-black/5 z-10",
+                className
+            )}
+        >
+            <Container className='flex items-center justify-between '>
+                <Categories />
+                <SortPopup />
+            </Container>
+        </div>
+    );
+};
+
+export default TopBar;
