20df60de87f50971f5addb6871f8f55f409ac50d
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2025-01-15 21:37:58 +0200

    add checkbox filters

---
+++ components/data/checkboxData.js
@@ -0,0 +1,9 @@
+export const checkboxData = [
+    { text: "солені огірки", value: "1" },
+    { text: "томати", value: "2" },
+    { text: "цибуля", value: "3" },
+    { text: "моцарелла", value: "4" },
+    { text: "бекон", value: "5" },
+    { text: "салямі", value: "6" },
+    { text: "гострий перець", value: "7" },
+]
---
+++ components/shared/checkbox-filters-group.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { FC } from "react";
+import FilterCheckbox, { FilterChecboxProps } from "./filter-checkbox";
+import { Input } from "../ui/input";
+
+// import { Skeleton } from '../ui';
+
+// type Item = FilterChecboxProps;
+
+type CheckboxFiltersGroup = {
+    title: string;
+    items: FilterChecboxProps[];
+    defaultItems?: FilterChecboxProps[];
+    limit?: number;
+    loading?: boolean;
+    searchInputPlaceholder?: string;
+    onClickCheckbox?: (id: string) => void;
+    defaultValue?: string[];
+    selected?: Set<string>;
+    className?: string;
+    name?: string;
+};
+
+export const CheckboxFiltersGroup: FC<CheckboxFiltersGroup> = ({
+    title,
+    items,
+    // defaultItems,
+    // limit = 5,
+    searchInputPlaceholder = "Пошук...",
+    className,
+    // loading,
+    onClickCheckbox,
+    selected,
+    name,
+}) => {
+    //  const list = showAll
+    //      ? items.filter((item) =>
+    //            item.text.toLowerCase().includes(searchValue.toLocaleLowerCase())
+    //        )
+    //      : (defaultItems || items).slice(0, limit);
+
+    return (
+        <div className={className}>
+            <p className='font-bold mb-3'>{title}</p>
+            <div className='mb-5'>
+                <Input
+                    // onChange={onChangeSearchInput}
+                    placeholder={searchInputPlaceholder}
+                    className='bg-gray-50 border-none'
+                />
+            </div>
+            <div className='flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar'>
+                {items.map((item, index) => (
+                    <FilterCheckbox
+                        key={index}
+                        text={item.text}
+                        value={item.value}
+                        // endAdornment={item.endAdornment}
+                        checked={selected?.has(item.value)}
+                        onCheckedChange={() => onClickCheckbox?.(item.value)}
+                        name={name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
