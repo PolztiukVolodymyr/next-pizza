78ee5d6c474233dd647bdb8f65acb6d9c337b177
Denys <denys.khmyra.work@gmail.com>
2025-03-14 18:34:12 +0200

    bugfix send email

--- lib/send-email.ts
+++ lib/send-email.ts
@@ -4,7 +4,7 @@ export const sendEmail = async (to: string, subject: string, template: React.Rea
 	const resend = new Resend(process.env.RESEND_API_KEY);
 
 	const {data, error} = await resend.emails.send({
-		from: "wowa.pol@ukr.net",
+		from: "onboarding@resend.dev",
 		to,
 		subject,
 		// text: "",
@@ -17,3 +17,10 @@ export const sendEmail = async (to: string, subject: string, template: React.Rea
 
 	return data;
 };
+
+// resend.emails.send({
+// 	from: "onboarding@resend.dev",
+// 	to: "wowa.pol@ukr.net",
+// 	subject: "Hello World",
+// 	html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
+// });
